114306baba6814dd0389812315cf1c494580614a
xief001 <fyphia@gmail.com>
2016-05-10 12:38:02 +0800

    hash_table.h

--- hash_table.h
+++ hash_table.h
@@ -23,24 +23,19 @@ typedef struct times_and_flag
 	int flag;
 }times_and_flag;
 
-typedef struct k_mer_node     //k-mer节点的结构
+typedef struct k_mer_node     
 {
 	int i_read;
 	int j_position;
 	struct k_mer_node *next;
 }k_mer_node;
 
-typedef struct head_node_index   //存储index的头节点结构
+typedef struct head_node_index   
 {
 	char *head_hash_table;
 	struct k_mer_node *next;
 }head_node_index;
 
-//extern int count_of_node[num_read][int(len_read*1.1)];//节点计数数组
-
-//extern int get_index_num(char *ch);
-//extern head_node_index head_of_hash_table[max_len_head_hash];//哈希表头节点数组
-//extern void output_hash_table();
 extern void hash_table();
 
 
