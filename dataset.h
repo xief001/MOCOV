a4e2222e63b90372a29f932a85bd43227ec2f792
xief001 <fyphia@gmail.com>
2016-05-10 12:38:42 +0800

    dataset.h

--- dataset.h
+++ dataset.h
@@ -10,60 +10,57 @@
 //#include"main.h"
 using namespace std;
 
+
 /*
-#define len_genome 1000000     //基因组长度为10000000
-#define num_read 5000           //随机起点个数为1000
-#define len_read 1000            //read 的长度 1K-10K
-#define short_k 7               //进行匹配的短k-mer长度
-#define long_k 20                // 进行匹配的长k-mer长度
-#define len_index 3              //进行匹配时的索引长度
-#define len_index_error 3        //匹配k-mer时允许有的错误匹配数
-#define len_array_k_mer 64         //k-mer存储数组的长度
-#define len_k_mer_error 3        //匹配时容许k-mer错误
-#define invalid_value  3            //无效匹配
-#define valid_value  3            //有效匹配
+#define len_genome 1000000       //genome size 10000000
+#define num_read 5000            //random read position 1000
+#define len_read 1000            //read length 1K-10K
+#define short_k 7                //k-mer length for short aligning
+#define long_k 20                // k-mer length for checking correction
+#define len_index 3              //index length for comparing k-mers in hash table with k-mers in read 
+#define len_index_error 3        //hamming distance between k-mers' index in hash table with k-mers in read
+#define len_k_mer_error 3        //hamming distance between k-mers in hash table with k-mers in read
+#define invalid_value  3         //if match value < invalid_value ,the match is invalid
+#define valid_value  3           //if match value > valid_value ,the match is valid
 */
 
-#define len_genome 4000000     //基因组长度为10000000
-#define num_read 1000         //随机起点个数为1000
-#define len_read 10000            //read 的长度 10-20
-#define short_k 10              //进行匹配的短k-mer长度，
-#define long_k 6               // 进行匹配的长k-mer长度
-#define len_index 3              //进行匹配时的索引长度
-#define len_index_error 3        //匹配k-mer时允许有的错误匹配数
-#define len_array_k_mer 64         //k-mer存储数组的长度
-#define len_k_mer_error 1        //匹配时容许k-mer错误
-#define invalid_value  2            //无效匹配
-#define valid_value  5//有效匹配
+#define len_genome 4000000       //genome size 10000000
+#define num_read 1000            //random read position 1000
+#define len_read 10000           //read length 1000-10000
+#define short_k 10               //k-mer length for short aligning
+#define long_k 6                 //k-mer length for checking correction
+#define len_index 3              //index length for comparing k-mers in hash table with k-mers in read 
+#define len_index_error 3        //hamming distance between k-mers' index in hash table with k-mers in read
+#define len_k_mer_error 1        //hamming distance between k-mers in hash table with k-mers in read
+#define invalid_value  2         //if match value < invalid_value ,the match is invalid
+#define valid_value  5           //if match value > valid_value ,the match is valid
 
 /*
-#define len_genome 1000  //基因组长度为10000000
-#define num_read 50         //随机起点个数为1000
-#define len_read 100            //read 的长度 10-20
-#define short_k 6              //进行匹配的短k-mer长度
-#define long_k 6               // 进行匹配的长k-mer长度
-#define len_index 3              //进行匹配时的索引长度
-#define len_index_error 3        //匹配k-mer时允许有的错误匹配数
-#define len_array_k_mer 64         //k-mer存储数组的长度
-#define len_k_mer_error 0        //匹配时容许k-mer错误
-#define invalid_value  2            //无效匹配
-#define valid_value  2//有效匹配
+#define len_genome 1000          //genome size 1000
+#define num_read 50              //random read position 50
+#define len_read 100             //read length 10-100
+#define short_k 6                //k-mer length for short aligning
+#define long_k 6                 //k-mer length for checking correction
+#define len_index 3              //index length for comparing k-mers in hash table with k-mers in read 
+#define len_index_error 3        //hamming distance between k-mers' index in hash table with k-mers in read
+#define len_k_mer_error 0        //hamming distance between k-mers in hash table with k-mers in read
+#define invalid_value  2         //if match value < invalid_value ,the match is invalid
+#define valid_value  2           //if match value > valid_value ,the match is valid
 
 */
 typedef struct k_mer
 {
 	//char* ch ;
-	string ch;                   //short_k长度k-mer
-	int count_ch;                //本k-mer出现次数
+	string ch;                   
+	int count_ch;                //the k-mer appearing times
 }k_mer;
 
-extern char genome[len_genome+1];//基因组字符串
-extern int position[num_read];//长read起始位置数组
-extern int length[num_read];//1000条read的随机长度。1K-10K
-extern char sample[num_read][int(len_read*2)];//存放长read，长度扩大为1.1倍，用来存放碱基插入错误
-extern int total_num_of_bases;//生成的长read中所有碱基总数
-extern k_mer array_k_mer[len_array_k_mer];
-extern int num_short_k_mer; //标记短k-mer组中出现的k-mer的数量
+extern char genome[len_genome+1];                  //genome
+extern int position[num_read];                     //array for long read starting positions
+extern int length[num_read];                       //array for every long read's length
+extern char sample[num_read][int(len_read*2)];     //array for store every long read with error
+extern int total_num_of_bases;                     //total num of bases in sample
+extern int num_short_k_mer;                        //number of k-mers for short match
 
 
 extern void dataset();
